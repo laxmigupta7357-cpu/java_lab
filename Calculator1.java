7d37225ba3b535b9c1ed6cf35da0a72fd86bed52
laxmigupta7357-cpu <laxmigupta7357@gmail.com>
2026-02-17 15:38:18 +0530

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Calculator1 {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter first number: ");
+int a = sc.nextInt();
+System.out.print("Enter second number: ");
+int b = sc.nextInt();
+sc.close();
+add(a,b);
+sub(a,b);
+mul(a,b);
+div(a,b);
+}
+public static void add(int a,int b){
+System.out.println("Addition: "+(a+b));
+}
+
+public static void sub(int a,int b){
+System.out.println("Subtraction: "+(a-b));
+}
+public static void mul(int a,int b){
+System.out.println("Multiplication: "+(a*b));
+}
+public static void div(int a,int b){
+if(b==0)
+System.out.println("Division not possible");
+else
+System.out.println("Division: "+(a/b));
+}
+}
