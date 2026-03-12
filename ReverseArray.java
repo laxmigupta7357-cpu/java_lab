70a601bb44b71b52fe357ed95153b8c03503268e
laxmigupta7357-cpu <laxmigupta7357@gmail.com>
2026-03-13 00:45:09 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,25 @@
+class ReverseArray
+{
+    void reverse()
+    {
+        int arr[] = {10, 20, 30, 40, 50};
+
+        System.out.println("Original Array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println("\nReversed Array:");
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String args[])
+    {
+        ReverseArray r = new ReverseArray();
+        r.reverse();
+    }
+}
